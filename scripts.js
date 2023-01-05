bf3ad33b23ff1d97fa96c23ac1fa244b34efbbaa
Micheal LeBlanc <mikeleblanc8489@gmail.com>
2023-01-04 20:24:21 -0500

    Button manipulation

--- scripts.js
+++ scripts.js
@@ -1,12 +1,3 @@
-/*
-    a <p> with red text that says “Hey I’m red!”
-    an <h3> with blue text that says “I’m a blue h3!”
-    a <div> with a black border and pink background color with the following elements inside of it:
-        another <h1> that says “I’m in a div”
-        a <p> that says “ME TOO!”
-        Hint for this one: after creating the <div> with createElement, append the <h1> and <p> to it before adding it to the container.
- */
-
 //Define constants to be reused       
 const container = document.querySelector('#body-container');
 const paragraph = document.createElement('p');
@@ -42,3 +33,9 @@ paragraphDescription.textContent = "ME TOO!"
 divCreate.appendChild(firstHeaderCreate);
 divCreate.appendChild(paragraphDescription);
 container.appendChild(divCreate);
+
+//button
+const testButton = document.querySelector('.test');
+testButton.addEventListener('click', () => {
+    alert("HOLY MOLY BROTHER")
+})
