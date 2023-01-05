185c805110e37a011e71b2027f1cb1727983bff2
Micheal LeBlanc <mikeleblanc8489@gmail.com>
2023-01-04 20:08:38 -0500

    Add more DOM elements

--- scripts.js
+++ scripts.js
@@ -10,7 +10,10 @@
 //Define constants to be reused       
 const container = document.querySelector('#body-container');
 const paragraph = document.createElement('p');
+const paragraphDescription = document.createElement('p');
 const thirdHeader = document.createElement('h3');
+const divCreate = document.createElement('div');
+const firstHeaderCreate = document.createElement('h1');
 
 //Adds red text
 paragraph.classList.add('red-text');
@@ -24,3 +27,18 @@ thirdHeader.textContent = "I'm a blue H3!";
 thirdHeader.style.color = 'blue';
 container.appendChild(thirdHeader);
 
+//Add div with a border and background
+divCreate.classList.add('cta-container');
+divCreate.setAttribute('style', 'border: solid 2px black; background-color: pink;');
+
+//Add subdivisions to cta-container
+firstHeaderCreate.classList.add('cta-header');
+firstHeaderCreate.textContent = "I'm in a div";
+
+paragraphDescription.classList.add('cta-description');
+paragraphDescription.textContent = "ME TOO!"
+
+//Append to DOM
+divCreate.appendChild(firstHeaderCreate);
+divCreate.appendChild(paragraphDescription);
+container.appendChild(divCreate);
